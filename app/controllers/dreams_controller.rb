de503d0bbf78a0126b5b20201548d252d5e5f6f8
AP-Lett <audreana.lett@gmail.com>
2025-03-18 21:38:51 +1000

    New, and Create working in local host

--- app/controllers/dreams_controller.rb
+++ app/controllers/dreams_controller.rb
@@ -8,9 +8,23 @@ class DreamsController < ApplicationController
   end
 
   def new
+    @dream = Dream.new
   end
 
   def create
+    @dream = Dream.new(
+      title: params["dream"][:title],
+      date: params["dream"][:date],
+      description: params["dream"][:description],
+      type_of_dream: params["dream"][:type_of_dream]
+    )
+
+    if @dream.save
+      redirect_to @dream, notice: 'Dream has successfully been added to the journal.'
+      flash[:notice] = 'Dream has successfully been added to the journal.'
+    else
+      render :new
+    end
   end
 
   def edit
